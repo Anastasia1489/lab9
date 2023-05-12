0139b996ac2e3f6a841397e44548327ef649d9cd
Anastasia1489 <125042588+Anastasia1489@users.noreply.github.com>
2023-05-12 13:18:16 +0300

    Update govno3.py

--- govno3.py
+++ govno3.py
@@ -6,7 +6,7 @@ with open(input_file) as csvfile:
     reader = csv.DictReader(csvfile)
     sm = 0
     for row in reader:
-        sm += row['Цена']
+        sm += row['Количество'] * row['Цена']
         print(row['Продукт'] + '-' + str(row['Количество']) + 'шт. за' + row['Цена'] + 'руб.')
 
     print('Итоговая сумма:' + sm + 'руб.')
