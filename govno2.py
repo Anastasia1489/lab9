008edcc3fb6442b119366451f49d385a45ab5e72
Anastasia1489 <125042588+Anastasia1489@users.noreply.github.com>
2023-05-23 13:37:16 +0300

    Update govno2.py

--- govno2.py
+++ govno2.py
@@ -4,18 +4,6 @@ from PIL import Image, ImageOps, ImageFilter, ImageDraw, ImageFont
 
 formats = ['.jpg', '.png']
 
-
-def check_extension(filename):
-    spl = os.path.splitext(filename)
-    return spl[1] not in formats
-
-
-def update_image(path, out_path):
-    im = Image.open(path)
-    edg = im.filter(ImageFilter.FIND_EDGES)
-    return (edg, im.filename)
-
-
 input_path = input()
 out_path = input()
 
@@ -33,17 +21,16 @@ os.chdir(input_path)
 pictures = []
 
 for fl in os.listdir():
-    if check_extension(fl):
+    if os.path.splitext(fl)[1] not in formats:
         print('cannot process fl: {}, skip it'.format(fl))
         continue
 
-    pictures.append(update_image(fl, out_path))
+    im = Image.open(path)
+    edg = im.filter(ImageFilter.FIND_EDGES)
+    pictures.append(update_image(edg, im.filename))
 
 
 os.chdir(start_dir)
 os.chdir(out_path)
-index = 0
 for pic in pictures:
-    pic[0].save(str(index) + '-' + pic[1])
-    index += 1
-
+    pic[0].save(pic[1])
